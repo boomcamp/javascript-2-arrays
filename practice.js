3328268c29ca13411a42c2067ec3b76a2b28c0fd
Jules <jules.ballaran@boom.camp>
2019-05-24 14:18:30 +0800

    PROBLEM 5 done

--- practice.js
+++ practice.js
@@ -90,8 +90,16 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-
-
+function evenFinder(nums){
+  var num = [];
+  for(n of nums){
+    if(n % 2 == 0){
+      num.push(n);
+    }
+  }
+  return num;
+}
+console.log(evenFinder(nums));
 
 ////////// PROBLEM 6 //////////
 
