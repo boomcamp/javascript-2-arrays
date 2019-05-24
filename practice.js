b1c057bbd19dfd33d6e44dbaba39f55166bafab1
Trizha Kate Longaza <trizha.longaza@boom.camp>
2019-05-24 15:56:05 +0800

    problem 4-5: solved

--- practice.js
+++ practice.js
@@ -52,8 +52,6 @@ var family = ['Aodhan', 'Haley', 'Finn', 'Reid'];
 */
 
 //Code Here
-
-
 function looper(family) {
   for (let i of family){
     alert(i);
@@ -73,6 +71,14 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+function reversedLooper(letters) {
+  for (let i = letters.length-1; i >= 0; i--) {
+    alert(letters[i]);
+  }
+}
+reversedLooper(letters);
+
+
 
 ////////// PROBLEM 5 //////////
 
@@ -86,6 +92,17 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+
+function evenFinder(num){
+  var temp = [];
+  for (let i = 0; i <= num.length; i++){
+    if (num[i] % 2 === 0){
+      temp.push(num[i]);
+    }
+  }
+  return temp;
+}
+evenFinder(num);
 
 
 
