a6c291950da8397cc1a84d53237370a20e88de97
John Paulo Barbin <john.barbin@boom.camp>
2019-05-24 15:59:00 +0800

    Finished Problem 4

--- practice.js
+++ practice.js
@@ -70,6 +70,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+function reversedLooper(letters) {
+  for (i = letters.length - 1; i >= 0; i--) {
+    alert(letters[i]);
+  }
+}
 
 
 
